f6b08d733cb7bc554ab535e0d809ffda073a42a4
Tulio Colares <tuliocsaraiva@gmail.com>
2024-03-02 14:25:24 -0300

    marminino

--- src/marminino.js
+++ src/marminino.js
@@ -1,5 +1,15 @@
 function executar(numero) {
-    // yourcode
+    while (numero <=100 ){
+        if (numero % 3 == 0 && numero % 4 ==0) {
+            return "MarMinino"
+        } else if (numero % 3 == 0) {
+            return "Mar"
+        } else if (numero % 4 == 0) {
+            return "Minino"
+        } 
+    }
+    numero++
 }
 
 module.exports = executar;
+
